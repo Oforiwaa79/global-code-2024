ec1993d4115f79defc7fc0bbfc155dc89ba947f7
Solomon Eshun <solomoneinsteinmit300@gmail.com>
2024-08-21 06:40:54 -0900

    Added bunch of conditions

--- week1/day3/input.py
+++ week1/day3/input.py
@@ -1,14 +1,20 @@
 name = input("What is your name? ")
 age = input("What is your age? ")
-# print(type(age))
 
-print("Hello " + name+  "! You are " + age + " old.")
+# check if age is an integer
+if age.isdigit():
+    age = int(age)
+    y = "years" if int(age) > 1 else "year"
 
-# y = "years" if int(age) > 1 else "year"
-y = "" 
-if int(age) > 1: 
-    y = "years"
+    if age < 0:
+        print("Invalid age")
+    # else:
+    elif 0 > age <= 10:
+        print(f"\nAt age {age}? You're a child dude")
+    elif  10 > age <= 18:
+        print(f"\nYoo {name} you're a teen. You're {age}{ y}.")
+    else:
+        print(f"\nWrite clean code dude, you're {age} {y}. Bela Chao {name.upper()}")
 else:
-    y = "year"
+    print("\nenter a number for age")
 
-print("Hello " + name + "! You are " + age + " " + y + " old.")
